dcc0396ebca5f94a72d5c7ae825004e544655c3f
jgrodriguez <jgrodriguez@reyma.com.mx>
2025-06-28 12:52:16 -0600

    Ms cambios en usuarios

--- PruebasGIT/Entidades/Usuarios.cs
+++ PruebasGIT/Entidades/Usuarios.cs
@@ -9,5 +9,7 @@ namespace PruebasGIT.Entidades
     public class Usuarios
     {
         public string Domicilio { get; set; }
+        public string Telefono { get; set; }
+        public int Edad {  get; set; }
     }
 }
